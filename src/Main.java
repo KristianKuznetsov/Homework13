00e8743e90ea0ce3701f2e6637adef8569c0ecda
KristianKuznetsov <black.sky.dragon1@gmail.com>
2024-05-04 20:52:58 +0300

    commit star task

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         SolutionBuilder.buildTask1();
+        SolutionBuilder.buildTaskStar();
     }
 }
--- src/SolutionBuilder.java
+++ src/SolutionBuilder.java
@@ -3,7 +3,7 @@ import task1.WrongLoginException;
 import task1.WrongPasswordException;
 
 public class SolutionBuilder {
-    public static void buildTask1(){
+    public static void buildTask1() {
         String login = "mylogin";
         String password = "mypassword1";
         String confirmPassword = "mypassword1";
@@ -15,9 +15,46 @@ public class SolutionBuilder {
             System.out.println("Ошибка валидации логина: " + e.getMessage());
         } catch (WrongPasswordException e) {
             System.out.println("Ошибка валидации пароля: " + e.getMessage());
-        }finally {
+        } finally {
             System.out.println("Валидация успешна: " + isValid);
         }
+    }
+
+    public static void buildTaskStar()  {
+        //Почему-то работает только так, я пытался через return и break как в коментариях ниже,
+        // но оно все равно сначала идет в finaly блок а только потом на оператор выхода
+        //Вобщем стопорить полностью программу, явно не то что от нас хотели, но условия задачи выполнены)
+        try {
+            //int a = 3/0;
+            System.out.println("Try block");
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("Catch block");
+            System.exit(0);
+        } finally {
+            System.out.println("Finally block");
+        }
+
+
+//        outer:
+//        {
+//            try {
+//                System.out.println("Try block");
+//                break outer;
+//            } finally {
+//                System.out.println("Finally block");
+//            }
+//        }
+
+//        try {
+//            System.out.println("Try block");
+//            return;
+//        } catch (Exception e) {
+//            System.out.println("Catch block");
+//            return;
+//        } finally {
+//            System.out.println("Finally block");
+//        }
 
     }
 }
